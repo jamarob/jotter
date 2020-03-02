3468c4c7efe76799ac8cba2fb327940153fdcec9
Jan Robert <janmartinrobert@gmail.com>
2020-03-02 15:15:55 +0100

    Move Note and NotesList to component folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import GlobalStyle from './GlobalStyle'
-import NotesList from './NotesList'
+import NotesList from './components/NotesList'
 import { getNotes } from './services'
 
 function App() {
