e6fa51c8a1984428881a8c8f1935769b734d5c9e
Jan Robert <janmartinrobert@gmail.com>
2020-03-02 15:34:50 +0100

    Move notes.json and services.js to data directory

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import GlobalStyle from './GlobalStyle'
 import NotesList from './components/NotesList'
-import { getNotes } from './services'
+import { getNotes } from './data/services'
 
 function App() {
   const notes = getNotes()
