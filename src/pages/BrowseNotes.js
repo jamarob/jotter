c9e4b32f4702faaa3657692f1ad7f364c8d2b48d
Jan Robert <janmartinrobert@gmail.com>
2020-03-04 21:56:31 +0100

    Add main container

--- src/pages/BrowseNotes.js
+++ src/pages/BrowseNotes.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Header from '../components/Header'
 import NotesList from '../components/NotesList'
+import styled from 'styled-components/macro'
+import Search from '../components/Search'
 
 export default function BrowseNotes({ notes, onTagClick }) {
   return (
     <>
       <Header title="Browse Notes" />
-      <NotesList notes={notes} onTagClick={onTagClick} />
+      <Main>
+        <NotesList notes={notes} onTagClick={onTagClick} />
+      </Main>
     </>
   )
 }
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+`
