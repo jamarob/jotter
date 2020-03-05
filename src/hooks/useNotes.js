c902b8d269e81a68169066c8b5919958b64d3d38
Jan Robert <janmartinrobert@gmail.com>
2020-03-05 09:29:29 +0100

    Update useNotes hook

--- src/hooks/useNotes.js
+++ src/hooks/useNotes.js
@@ -5,10 +5,12 @@ export default function useNotes() {
   // eslint-disable-next-line
   const [originalNotes, setOriginalNotes] = useState(getNotes())
   const [notes, setNotes] = useState(originalNotes)
+  const [searchTerm, setSearchTerm] = useState('')
 
   function searchNotes(string) {
+    setSearchTerm(string)
     setNotes(originalNotes.filter(note => note.text.includes(string)))
   }
 
-  return { notes, searchNotes }
+  return { notes, searchNotes, searchTerm }
 }
