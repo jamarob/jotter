450b63232671ac43cdc6cc4c5a38580b1c5c2d29
Jan Robert <janmartinrobert@gmail.com>
2020-03-06 17:23:33 +0100

    Add note form component

---
+++ src/components/NoteForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+
+export default function NoteForm({ onSave }) {
+  const [noteText, setNoteText] = useState('')
+  const history = useHistory()
+  return (
+    <StyledNoteForm>
+      <textarea
+        onChange={event => setNoteText(event.target.value)}
+        value={noteText}
+      ></textarea>
+      <button className="cancel" onClick={handleCancel}>
+        Cancel
+      </button>
+      <button className="save" onClick={handleSave}>
+        Save
+      </button>
+    </StyledNoteForm>
+  )
+
+  function handleCancel() {
+    history.push('/')
+  }
+
+  function handleSave() {
+    const text = noteText.trim()
+    if (text) {
+      const created = Date.now()
+      onSave({
+        text,
+        created,
+      })
+      history.push('/')
+    }
+  }
+}
+
+const StyledNoteForm = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto 48px;
+  grid-template-areas:
+    'text text'
+    'cancel save';
+
+  textarea {
+    padding: 8px;
+    grid-area: text;
+    font-size: 18px;
+  }
+
+  button {
+    margin: 8px;
+    border-radius: 8px;
+    font-weight: bold;
+    font-size: 18px;
+  }
+
+  .cancel {
+    color: #333;
+    border-color: #333;
+    background: whitesmoke;
+  }
+
+  .save {
+    color: whitesmoke;
+    border-color: #333;
+    background: #333;
+  }
+`
