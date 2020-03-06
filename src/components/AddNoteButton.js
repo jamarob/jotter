11b886678ebcd398a25b8b84e5d901cd88848cf4
Jan Robert <janmartinrobert@gmail.com>
2020-03-06 15:16:24 +0100

    Add floating action button

---
+++ src/components/AddNoteButton.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+import { Link } from 'react-router-dom'
+
+export default function AddNoteButton() {
+  return <StyledAddNoteButton to="/add">+</StyledAddNoteButton>
+}
+
+const StyledAddNoteButton = styled(Link)`
+  position: absolute;
+  width: 48px;
+  height: 48px;
+  bottom: 0;
+  right: 0;
+  margin: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+
+  border-radius: 50%;
+  background: #333;
+  opacity: 0.8;
+  color: white;
+
+  font-weight: bold;
+  font-size: 36px;
+  cursor: pointer;
+`
