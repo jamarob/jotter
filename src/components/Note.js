2986b297fae197715518d1273ce370dac3c36a81
Jan Robert <janmartinrobert@gmail.com>
2020-03-10 14:56:15 +0100

    Add delete and edit buttons

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,17 +3,30 @@ import styled from 'styled-components/macro'
 import PropTypes from 'prop-types'
 import DateView from './DateView'
 import { replaceTags } from './Tag'
+import { MdDelete, MdModeEdit } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 Note.propTypes = {
+  id: PropTypes.string.isRequired,
   created: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
   onTagClick: PropTypes.func.isRequired,
 }
 
-export default function Note({ created, text, onTagClick }) {
+export default function Note({ id, created, text, onTagClick }) {
   return (
     <StyledNote>
-      <DateView time={created} />
+      <NoteHeader>
+        <DateView time={created} />
+        <ActionButtons>
+          <Link to={'/delete/' + id}>
+            <MdDelete />
+          </Link>
+          <Link to={'/edit/' + id}>
+            <MdModeEdit />
+          </Link>
+        </ActionButtons>
+      </NoteHeader>
       {replaceTags(text, onTagClick)}
     </StyledNote>
   )
@@ -23,4 +36,19 @@ const StyledNote = styled.section`
   box-shadow: 2px 2px 2px 2px lightgrey;
   font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
   padding: 8px;
+`
+
+const NoteHeader = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const ActionButtons = styled.div`
+  margin-left: auto;
+
+  * {
+    margin-left: 8px;
+    color: gray;
+    font-size: 24px;
+  }
 `
