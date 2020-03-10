b3bc36f25b2d92b592bed3db9d23eee5bc90c919
Jan Robert <janmartinrobert@gmail.com>
2020-03-10 17:32:40 +0100

    Move add note logic out of NoteForm

--- src/pages/AddNotePage.js
+++ src/pages/AddNotePage.js
@@ -11,7 +11,12 @@ export default function AddNotePage({ onAddNote }) {
   return (
     <>
       <Header title="Add Note" />
-      <NoteForm onSave={onAddNote} />
+      <NoteForm onSave={handleSave} />
     </>
   )
+
+  function handleSave(text) {
+    const created = Date.now()
+    onAddNote({ text, created })
+  }
 }
