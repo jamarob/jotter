3295703fdd19e26c0f0581ea382ccb81267293dd
Jan Robert <janmartinrobert@gmail.com>
2020-03-19 16:02:42 +0100

    Merge and move services to util

---
+++ src/hooks/useSearch.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import { loadSearchTerm, saveSearchTerm } from '../util/services'
+import filterNotes from '../util/filterNotes'
+
+export default function useSearch(notes) {
+  const [filtered, setFiltered] = useState(notes)
+  const [search, setSearch] = useState(loadSearchTerm())
+
+  useEffect(() => {
+    setSearch(search)
+    setFiltered(filterNotes(notes, search))
+    saveSearchTerm(search)
+  }, [search, notes])
+
+  return [search, setSearch, filtered]
+}
