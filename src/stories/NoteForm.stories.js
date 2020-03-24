6c52200c2d7269622a714e5215f3c65680c0e4fd
Jan Robert <janmartinrobert@gmail.com>
2020-03-24 17:46:09 +0100

    Add craete and edit variant

--- src/stories/NoteForm.stories.js
+++ src/stories/NoteForm.stories.js
@@ -8,11 +8,23 @@ export default {
   component: NoteForm,
 }
 
-export const Default = () => (
+export const Create = () => (
   <>
     <GlobalStyle />
     <div style={{ padding: 24 }}>
       <NoteForm onSave={action('Save')} />
+    </div>
+  </>
+)
+
+export const Edit = () => (
+  <>
+    <GlobalStyle />
+    <div style={{ padding: 24 }}>
+      <NoteForm
+        onSave={action('Save')}
+        text="Lorem ipsum dolor sit amet, @consetetur sadipscing elitr, @sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At @vero eos et accusam et justo duo dolores et ea rebum. Stet clita @kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+      />
     </div>
   </>
 )
