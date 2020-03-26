d86704f9c8c1584caa2c0cec3ed72003079ae1c7
Jan Robert <janmartinrobert@gmail.com>
2020-03-26 21:19:25 +0100

    Add error boundary

--- src/util/services.js
+++ src/util/services.js
@@ -5,21 +5,35 @@ const SEARCH_TERM_KEY = 'SEARCH_TERM'
 const SERVER_URL = 'http://localhost:8080'
 
 export function getNotes() {
-  return axios.get(`${SERVER_URL}/notes`).then(response => response.data)
+  return axios
+    .get(`${SERVER_URL}/notes`)
+    .then(promiseStatus200)
+    .then(response => response.data)
 }
 
 export function postNote(note) {
-  return axios.post(`${SERVER_URL}/notes`, note).then(response => response.data)
+  return axios
+    .post(`${SERVER_URL}/notes`, note)
+    .then(promiseStatus200)
+    .then(response => response.data)
 }
 
 export function deleteNote(id) {
-  return axios.delete(`${SERVER_URL}/note/${id}`)
+  return axios.delete(`${SERVER_URL}/note/${id}`).then(promiseStatus200)
 }
 
 export function putNote(note) {
   return axios
     .put(`${SERVER_URL}/note/${note.id}`, note)
+    .then(promiseStatus200)
     .then(response => response.data)
+}
+
+function promiseStatus200(response) {
+  return new Promise((resolve, reject) => {
+    response.status === 200 && resolve(response)
+    reject(`Response status: ${response.status}`)
+  })
 }
 
 export function loadSearchTerm() {
