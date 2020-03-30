405c42a05c17a4b73dccddaff1268c3f0760d47c
Jan Robert <janmartinrobert@gmail.com>
2020-03-31 00:38:15 +0200

    Remove main nesting and clean up prop types

--- src/components/Note.js
+++ src/components/Note.js
@@ -8,8 +8,8 @@ import IconButton from './Buttons/IconButton'
 
 Note.propTypes = {
   id: PropTypes.string.isRequired,
-  created: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  edited: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  created: PropTypes.string.isRequired,
+  edited: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   onTagClick: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
@@ -43,6 +43,10 @@ export default function Note({
 const StyledNote = styled.section`
   margin: 0 0 var(--size-5) 0;
   padding: var(--size-2);
+
+  :last-child {
+    margin-bottom: var(--size-6);
+  }
 
   box-shadow: var(--shadow);
 
