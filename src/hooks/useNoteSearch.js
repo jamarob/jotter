2e3e783c4192d25e3b0e3f28a351baebaa3de522
Jan Robert <janmartinrobert@gmail.com>
2020-03-31 21:20:29 +0200

    Rename useSearch to useNoteSearch

--- src/hooks/useNoteSearch.js
+++ src/hooks/useNoteSearch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import filterNotes from '../util/filterNotes'
 
-export default function useSearch(notes) {
+export default function useNoteSearch(notes) {
   const [filtered, setFiltered] = useState(notes)
   const [search, setSearch] = useState('')
 
