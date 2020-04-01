68ad47e3069cca0b937da6f6a052046b9c641f83
Jan Robert <janmartinrobert@gmail.com>
2020-04-02 01:16:09 +0200

    Simplify App structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom'
-import styled from 'styled-components/macro'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import useNotes from './hooks/useNotes'
 import AddNotePage from './pages/AddNotePage'
 import BrowseNotes from './pages/BrowseNotes'
@@ -26,54 +20,32 @@ export default function App() {
     dismissUndo,
   } = useNotes()
   return (
-    <Router>
-      <PageLayout>
-        <Switch>
-          <Route path="/notes">
-            <BrowseNotes
-              status={status}
-              notes={notes}
-              onSearch={searchNotes}
-              searchTerm={searchTerm}
-              lastOperation={lastOperation}
-              undoLastOperation={undoLastOperation}
-              dismissUndo={dismissUndo}
-              onDelete={deleteNote}
-            />
-          </Route>
-          <Route path="/add">
-            <AddNotePage onAddNote={addNote} status={status} />
-          </Route>
-          <Route path="/edit/:id">
-            <EditNotePage
-              findNote={findNote}
-              updateNote={updateNote}
-              status={status}
-            />
-          </Route>
-          <Route path="/">
-            <Redirect to="/notes" />
-          </Route>
-        </Switch>
-      </PageLayout>
-    </Router>
+    <Switch>
+      <Route path="/notes">
+        <BrowseNotes
+          status={status}
+          notes={notes}
+          onSearch={searchNotes}
+          searchTerm={searchTerm}
+          lastOperation={lastOperation}
+          undoLastOperation={undoLastOperation}
+          dismissUndo={dismissUndo}
+          onDelete={deleteNote}
+        />
+      </Route>
+      <Route path="/add">
+        <AddNotePage onAddNote={addNote} status={status} />
+      </Route>
+      <Route path="/edit/:id">
+        <EditNotePage
+          findNote={findNote}
+          updateNote={updateNote}
+          status={status}
+        />
+      </Route>
+      <Route path="/">
+        <Redirect to="/notes" />
+      </Route>
+    </Switch>
   )
 }
-
-const PageLayout = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-
-  header {
-    flex-shrink: 0;
-  }
-
-  main {
-    flex-grow: 1;
-  }
-`
