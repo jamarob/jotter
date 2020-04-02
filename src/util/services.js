5e975f298ad21eb1a9f85963656410aee8d469ee
Jan Robert <janmartinrobert@gmail.com>
2020-04-02 10:20:27 +0200

    Add process.env

--- src/util/services.js
+++ src/util/services.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const SERVER_URL = 'http://localhost:8080'
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:8080'
 
 export function putNotes(notes) {
   return axios
