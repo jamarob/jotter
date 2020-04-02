c648b1ab72ae3dd885747da419c0929a78fe272b
Jan Robert <janmartinrobert@gmail.com>
2020-04-02 10:48:37 +0200

    Decorate actions

--- src/stories/SearchBar.stories.js
+++ src/stories/SearchBar.stories.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import { action } from '@storybook/addon-actions'
-import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+import React, { useState } from 'react'
+import { decorate } from '@storybook/addon-actions'
 import StoryContainer from './StoryContainer'
 import SearchBar from '../components/SearchBar'
 
 export default {
   title: 'SearchBar',
   component: SearchBar,
-  decorators: [withKnobs],
 }
 
-export const Default = () => (
-  <StoryContainer>
-    <SearchBar
-      folded={boolean('Folded', true)}
-      toggleFolded={action('Toggle fold')}
-      searchTerm={text('Search term', 'Foobar')}
-      onSearch={action('Search')}
-    />
-  </StoryContainer>
-)
+export const Default = () => {
+  const [folded, setFolded] = useState(true)
+  const [search, setSearch] = useState('Foobar')
+
+  const doSearch = decorate([_ => [search]])
+
+  const clearSearch = decorate([
+    _ => {
+      setSearch('')
+      return ['']
+    },
+  ])
+
+  const toggleFolded = decorate([
+    _ => {
+      const toggled = !folded
+      setFolded(toggled)
+      return [toggled]
+    },
+  ])
+
+  return (
+    <StoryContainer>
+      <SearchBar
+        folded={folded}
+        toggleFolded={toggleFolded.action('Toggle folded')}
+        search={search}
+        setSearch={setSearch}
+        onSearch={doSearch.action('Search')}
+        onClear={clearSearch.action('Clear search')}
+      />
+    </StoryContainer>
+  )
+}
